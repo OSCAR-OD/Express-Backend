9ac575783928a5ef905c25ee1d01ec0cbbbcb8da
OSCAR DEB <20-42888-1@Student.aiub.edu>
2023-08-05 21:42:20 +0600

    first3

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -2,6 +2,7 @@ const allowedOrigins = [
      'https://cityhub.onrender.com',
      'https://cityhub.onrender.com/',
      'http://cityhub.onrender.com/',
+     'https://cityhub.onrender.com/dash',
      'https://cityhub-dashboard.vercel.app',
      
     
