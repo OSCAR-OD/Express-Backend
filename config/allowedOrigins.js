747360e94645c960e708fd8f58e94536690523ae
OSCAR DEB <20-42888-1@Student.aiub.edu>
2023-08-05 21:28:48 +0600

    first2

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,6 +1,9 @@
 const allowedOrigins = [
-     'https://cityhub-dashboard.vercel.app/',
-     'https://cityhub.onrender.com'
+     'https://cityhub-dashboard.vercel.app',
+     'https://cityhub.onrender.com',
+     'https://cityhub.onrender.com/',
+     'http://cityhub.onrender.com/',
+    
 ]
 
 module.exports = allowedOrigins
